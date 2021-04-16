f94cecd25696412dcc4fa4c5cf597a88c4728e4e
tintoll <belselios@gmail.com>
2021-04-17 06:12:47 +0900

    가격책정부분 조건문을 재정의

--- src/refactoring/chap01/Rental.java
+++ src/refactoring/chap01/Rental.java
@@ -17,33 +17,10 @@ public class Rental {
     }
 
     double getCharge() {
-        double result = 0;
-        switch (getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (getDaysRented() > 2) {
-                    result += (getDaysRented() - 2) * 1.5;
-                }
-                break;
-            case Movie.NEW_RELEASE:
-                result += getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (getDaysRented() > 3) {
-                    result += (getDaysRented() - 3) * 1.5;
-                }
-                break;
-        }
-        return result;
+        return _movie.getCharge(_daysRented);
     }
     // 최신물을 이틀 이상 대여하면 2포인트 지급하고 그 외엔 1포인트 지급
     int getFrequentRenterPoints() {
-        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE)
-                && getDaysRented() > 1) {
-            return 2;
-        } else {
-            return 1;
-        }
+        return _movie.getFrequentRenterPoints(_daysRented);
     }
 }
