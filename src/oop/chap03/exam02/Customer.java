6938f9f54a282a73387bd9574890f26c88c2fe0a
tintoll <belselios@gmail.com>
2021-04-23 14:50:31 +0900

    오버라이딩 및 형변환 예제로 변경

--- src/oop/chap03/exam02/Customer.java
+++ src/oop/chap03/exam02/Customer.java
@@ -8,10 +8,20 @@ public class Customer {
     int bonusPoint;
     double bonusRatio;
 
-    public Customer() {
+//    public Customer() {
+//        customerGrade = "SILVER";
+//        bonusRatio = 0.01;
+//    }
+    // 디폴트 생성자 없애고 매개 변수가 있는 생성자 추가
+    public Customer(int customerID, String customerName) {
+        this.customerID = customerID;
+        this.customerName = customerName;
+
         customerGrade = "SILVER";
         bonusRatio = 0.01;
+//        System.out.println("Customer(int, String) 생성자 호출");
     }
+
 
     public int calcPrice(int price) {
         bonusPoint += price * bonusRatio;
--- src/oop/chap03/exam02/VIPCustomer.java
+++ src/oop/chap03/exam02/VIPCustomer.java
@@ -5,11 +5,29 @@ public class VIPCustomer extends Customer{
     private int agentID;
     double salesRatio;
 
-    public VIPCustomer() {
-        customerGrade = "VIP";    // 상위 클래스를 멤버변수를 변경하려면 protected로 되어 있어야 한다.
+//    public VIPCustomer() {
+//        customerGrade = "VIP";    // 상위 클래스를 멤버변수를 변경하려면 protected로 되어 있어야 한다.
+//        bonusRatio = 0.05;
+//        salesRatio = 0.1;
+//    }
+
+    // super를 이용하여 상위 클래스의 생성자 명시적으로 호출
+    public VIPCustomer(int customerID, String customerName) {
+        super(customerID, customerName);
+
+        customerGrade = "VIP";
         bonusRatio = 0.05;
         salesRatio = 0.1;
+
+//        System.out.println("VIPCustomer(int, String) 생성자 호출");
     }
+
+    @Override
+    public int calcPrice(int price) {
+        bonusPoint += price * bonusRatio;
+        return price - (int)(price * salesRatio);
+    }
+
 
     public int getAgentID() {
         return agentID;
